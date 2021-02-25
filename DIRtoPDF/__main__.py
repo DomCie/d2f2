50b8b605d3d3d21c2b0c7dfe8014cb8f705ee3ab
DomCie <43796756+DomCie@users.noreply.github.com>
2021-02-25 01:39:33 +0100

    Minor text and code corrections, add CLI option to enter shell

--- DIRtoPDF/__main__.py
+++ DIRtoPDF/__main__.py
@@ -1,4 +1,5 @@
 import DIRtoPDF.convert as convert
+import DIRtoPDF.shell as shell
 import getopt
 import os
 import sys
@@ -6,33 +7,37 @@ import sys
 
 def main(argv: list) -> None:
     help_msg = f"DIRtoPDF [OPTIONS] PATH [PATH...]\nConvert image folders to PDF files\n\nOPTIONS:\n-H, " \
-               f"--help\t\t\tShow this help\n-M, --multiple\t\t\tConvert each folder within PATH to a PDF file\n-O, " \
-               f"--output OUTPUT_PATH\tSave PDF files to OUTPUT_PATH instead of the current directory"
+               f"--help\t\t\t\t\tShow this help\n-M, --multiple\t\t\t\tConvert each folder within PATH to a PDF " \
+               f"file\n-S, --shell\t\t\t\t\tOpen interactive shell\n-O, --output OUTPUT_PATH\tSave PDF files to " \
+               f"OUTPUT_PATH instead of the current directory "
     try:
-        opts, args = getopt.getopt(argv[1:], 'HMO:', ['help', 'multiple', 'output='])
+        opts, args = getopt.getopt(argv[1:], 'HMSO:', ['help', 'multiple', 'shell', 'output='])
     except getopt.GetoptError:
-        print(help_msg)
-        sys.exit(2)
-
-    if len(args) == 0:
         print(help_msg)
         sys.exit(2)
 
     output_path = os.getcwd()
     is_multiple = False
 
-    for opt in opts:
-        if opt[0] in ('-H', '--help'):
+    for opt, par in opts:
+        if opt in ('-H', '--help'):
             print(help_msg)
             sys.exit(0)
-        elif opt[0] in ('-M', '--multiple'):
+        elif opt in ('-M', '--multiple'):
             is_multiple = True
-        elif opt[0] in ('-O', '--output'):
-            if os.path.exists(opt[1]):
-                output_path = opt[1]
+        elif opt in ('-S', '--shell'):
+            shell.start()
+            sys.exit(0)
+        elif opt in ('-O', '--output'):
+            if os.path.exists(par):
+                output_path = par
             else:
-                print(f"error: output path '{opt[1]}' doesn't exist", file=sys.stderr)
+                print(f"error: output path {par} doesn't exist", file=sys.stderr)
                 sys.exit(1)
+
+    if len(args) == 0:
+        print(help_msg)
+        sys.exit(2)
 
     for arg in args:
         if os.path.exists(arg):
@@ -41,7 +46,7 @@ def main(argv: list) -> None:
             else:
                 convert.single(arg, output_path)
         else:
-            print(f"skipping \"{arg}\", invalid path...")
+            print(f"warning: skipping \"{arg}\", invalid path...")
 
 
 if __name__ == "__main__":
